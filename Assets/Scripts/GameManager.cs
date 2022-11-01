d081437b2e9b4921fcc427c674a8cb18cf352325
berrynul <nfonken@ucsc.edu>
2022-10-31 21:48:33 -0700

    Slight Adjustment to game manager
    -fixedDeltatime instead of deltaTime

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        stopwatch += Time.deltaTime;
+        stopwatch += Time.fixedDeltaTime;
 
         if (stopwatch > timer)
         {
