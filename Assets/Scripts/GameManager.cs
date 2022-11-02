9890502f2feff1ae16581900effac16394deb435
berrynul <nfonken@ucsc.edu>
2022-11-01 17:41:02 -0700

    Added Light Flicker, Fixed Bugs
    -Added a light flicker, simply add it to a light and adjust the values as needed,
    the effect of value change is explained shortly in comments
    -Fixed a bug with CameraShift where it raised a reference error after the timer finished.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        stopwatch += Time.fixedDeltaTime;
+        stopwatch += Time.deltaTime;
 
         if (stopwatch > timer)
         {
