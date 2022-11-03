852da9f2775b3ddd9e49e919d0f1cf27f1df5617
juliekhou <juliekhou@yahoo.com>
2022-11-03 12:53:18 -0700

    fixed capsule spelling

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,7 +23,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Capusule");
+        player = GameObject.Find("Capsule");
     }
 
     // Update is called once per frame
